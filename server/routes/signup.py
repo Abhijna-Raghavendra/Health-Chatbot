3bd5f87290d18b09b9539a00ce7f5a404072a349
rockingpenny4 <rockingpenny4@gmail.com>
2024-06-17 14:08:54 +0530

    rfac:adds signup

--- server/routes/signup.py
+++ server/routes/signup.py
@@ -1,3 +1,43 @@
+from flask import request, jsonify
+import PyMongo
+from werkzeug import generate_password_hash
+import os
+
+mongodb_uri = os.environ['MONGODB_URI']
+mongo = PyMongo()
+
 def signup():
-    # Add signup here
-    return 
+    data = request.json  
+    username = data.get('username')
+    password = data.get('pswd') 
+    name = data.get('name')
+    dob = data.get('dob')
+    gender = data.get('gender')
+    chronic_conditions = data.get('chronic-conditions')
+    medications = data.get('medications')
+    surgeries = data.get('surgeries')
+    allergies = data.get('allergies')
+    family_history = data.get('family-history')
+    smoking = data.get('smoking')
+    alcohol = data.get('alcohol')
+
+    hashed_password = generate_password_hash(password)
+
+    user_data = {
+        'username': username,
+        'password': hashed_password,  
+        'name': name,
+        'dob': dob,
+        'gender': gender,
+        'chronic_conditions': chronic_conditions,
+        'medications': medications,
+        'surgeries': surgeries,
+        'allergies': allergies,
+        'family_history': family_history,
+        'smoking': smoking,
+        'alcohol': alcohol
+    }
+
+    mongo.db.users.insert_one(user_data)
+
+    return jsonify({'message': 'Signup successful'}), 201
